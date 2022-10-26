d8329404ea72d9d712e42d29e43f0c2ebde54cd9
Md Rakibul Hasan <rakibhasan0308@gmail.com>
2022-10-26 13:10:01 +0530

    Add dynamic category

--- index.js
+++ index.js
@@ -5,14 +5,22 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const courseCategory = require("./data/course-categories.json");
+const courseCategoryName = require("./data/course-categories.json");
+
+const courses = require("./data/courses.json");
 
 app.get("/", (req, res) => {
   res.send("News API Running");
 });
 
 app.get("/course_category", (req, res) => {
-  res.send(courseCategory);
+  res.send(courseCategoryName);
+});
+
+app.get("/course-item/course-details/:id", (req, res) => {
+  const id = req.params.id;
+  const allCourses = courses.filter((course) => course.course_id === id) || {};
+  res.send(allCourses);
 });
 
 app.listen(port, () => {
